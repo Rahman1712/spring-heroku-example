7aab4f557e02071dde26748c00dffbbfeba673f3
Rahman1712 <78753133+Rahman1712@users.noreply.github.com>
2021-09-08 00:52:03 +0530

    Add files via upload

---
+++ src/main/java/com/ar/example/SpringHerokuExampleApplication.java
@@ -0,0 +1,27 @@
+package com.ar.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringHerokuExampleApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Welocome to Heroku Cloud Solution Area";
+	}
+	
+	@GetMapping("/message/{input}")
+	public String getMessage(@PathVariable String input) {
+		return "Welocome "+input;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuExampleApplication.class, args);
+	}
+
+}
