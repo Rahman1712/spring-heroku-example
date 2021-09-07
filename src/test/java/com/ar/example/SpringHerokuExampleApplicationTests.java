07c93ffa2eaa67fee709576a204de9fb27745649
Rahman1712 <78753133+Rahman1712@users.noreply.github.com>
2021-09-08 00:39:21 +0530

    Add files via upload

---
+++ src/test/java/com/ar/example/SpringHerokuExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
